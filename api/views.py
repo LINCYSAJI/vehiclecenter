6e95a472dfbcb640995b5c775fa5067ea8c8673e
LINCYSAJI <104355791+LINCYSAJI@users.noreply.github.com>
2024-06-30 14:27:19 +0530

    add project servicecenter

---
+++ api/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+
+from rest_framework.viewsets import ModelViewSet
+
+from rest_framework.response import Response
+
+from rest_framework import authentication,permissions
+
+from rest_framework.generics import CreateAPIView,RetrieveUpdateDestroyAPIView
+
+from api.serializers import CustomerSerializer,WorkSerializer
+
+from api.models import Customer,Work
+
+class CustomerViewSetView(ModelViewSet):
+
+    serializer_class=CustomerSerializer
+
+    queryset=Customer.objects.all()
+    
+    authentication_classes=[authentication.TokenAuthentication]
+    
+    permission_classes=[permissions.IsAdminUser]
+    
+    def perform_create(self, serializer):
+        
+        serializer.save(technician=self.request.user)
+        
+    # def add_work(self,request,*args, **kwargs):
+        
+    #     id=kwargs.get("pk")
+        
+    #     customer_object=Customer.objects.get(id=id)
+        
+    #     serializer_instance=WorkSerializer(data=request.data)
+        
+    #     if serializer_instance.is_valid():
+
+    #         serializer_instance.save(customer=customer_object)
+
+    #         return Response(data=serializer_instance.data)
+
+    #     else:
+
+    #         return Response(data=serializer_instance.errors)
+    
+class WorkCreateView(CreateAPIView):
+    
+    serializer_class=WorkSerializer
+    
+    authentication_classes=[authentication.TokenAuthentication]
+    
+    permission_classes=[permissions.IsAdminUser]
+    
+    def perform_create(self, serializer):
+        
+        id=self.kwargs.get("pk")
+        
+        customer_instance=Customer.objects.get(id=id)
+        
+        serializer.save(customer=customer_instance)
+
+class WorkDetailView(RetrieveUpdateDestroyAPIView):
+
+    serializer_class=WorkSerializer
+    
+    queryset=Work.objects.all()
+    
+    authentication_classes=[authentication.TokenAuthentication]
+    
+    permission_classes=[permissions.IsAdminUser]
